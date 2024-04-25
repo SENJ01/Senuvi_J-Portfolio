047ee9a98feaddfe9f032e944aefb6bb95d6f596
Senuvi Jayasinghe <senuvijayasinghe@gmail.com>
2024-04-25 15:33:25 +0530

    changing the layout

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import '../css/Home.css'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import image1 from "../Images/4.webp";
@@ -8,13 +9,25 @@ import image5 from "../Images/linkedin.webp";
 import image6 from "../Images/github-alt.webp";
 import image7 from "../Images/instagram-.webp";
 
+import {
+    MDBCard,
+    MDBCardImage,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardLink,
+    MDBCardText,
+    MDBRow,
+    MDBCol
+  } from 'mdb-react-ui-kit';
+  
 
 
-function Home(){
+function Testing(){
     return(
         <section className="section">
-            <div className="container">
-                <div className="row">
+
+        
+<div className="Home-hero">
                     <div className="col-md-12 text-center">
                         <h1 className="main-heading">Hi, I'm Senuvi!</h1>
                         <h1 className="main-heading first-heading">Computer Science Undergraduate</h1>
@@ -23,69 +36,114 @@ function Home(){
                         <p className="main-body">I’m interested in UI/UX design and Front end web development and driven by innovation and problem-solving
                          with a knack for blending technology with user-centric design</p>
 
-                         <Button variant="primary" style={{backgroundColor: "#ff9933",color: "#0C2D57", borderRadius: "30px", margin:"10px", width:"150px", height:"50px", 
+                         <Button variant="primary" style={{backgroundColor: "#ff9933",color: "#0C2D57", borderRadius: "30px", margin:"10px", width:"150px",
                          border:"none", fontFamily:"montserrat", fontSize:"20px",  fontWeight:"bold"}} 
                          href="https://www.linkedin.com/in/senuvi-jayasinghe-414513256/">Let's chat</Button>  
                     </div>
                 </div>
             
-
-                {/* New Projects Section */}
-                <h1 className="project-heading">Projects</h1> {/* Updated heading */}
-                <div className="row projects justify-content-center"> {/* Center align the cards */}
-                    <div className="col-md-12"> {/* Add margin between cards */}
-                        <Card className="project-card" style={{border:"none", outline:"none"}}>
-                        <a href="https://www.figma.com/file/NshEau6BrO6uVld9NHiQqE/Website?type=design&node-id=83-176&mode=design" target="_blank" rel="noopener noreferrer">
-                            <Card.Img variant="top" src={image1} />
-                        </a>
-                            <Card.Body>
-                                <Card.Title style={{fontWeight:"bold", marginTop:"20px", color: "#0C2D57"}}>Ezthara Hair Miracle - Redesigned the UI of Ezthara</Card.Title>
-                                <Card.Link style={{textDecoration:"none", fontWeight:"500", fontSize:"15px", color:"#ff9933"}} href="https://www.figma.com/file/NshEau6BrO6uVld9NHiQqE/Website?type=design&node-id=83-176&mode=design">See more..</Card.Link>
-                            </Card.Body>
-                        </Card>
-                        <Card className="project-card" style={{border:"none", outline:"none"}}>
-                            
-                            <a href="https://www.figma.com/file/LAiqAojHVT9xcNFRA9gCkz/RevolUX?type=design&node-id=0%3A1&mode=design&t=ge2bQLo9LcW5VT6g-1" target="_blank" rel="noopener noreferrer">
-                            <Card.Img variant="top" src={image2}  />
-                            </a>
-                            <Card.Body>
-                                <Card.Title style={{fontWeight:"bold", marginTop:"20px", color: "#0C2D57"}}>BIT LEARN - Cryptocurrency Learning Application</Card.Title>
-                                <Card.Link style={{textDecoration:"none", fontWeight:"500", fontSize:"15px", color:"#ff9933"}} href="https://www.figma.com/file/LAiqAojHVT9xcNFRA9gCkz/RevolUX?type=design&node-id=0%3A1&mode=design&t=ge2bQLo9LcW5VT6g-1">See more..</Card.Link>
-                            </Card.Body>
-                        </Card>
-                        <Card className="project-card" style={{border:"none", outline:"none"}}>
-                        <a href="https://www.figma.com/file/NshEau6BrO6uVld9NHiQqE/Website?type=design&node-id=167-1424&mode=design" target="_blank" rel="noopener noreferrer">
-                            <Card.Img variant="top" src={image3}  />
-                            </a>
-                            <Card.Body>
-                                <Card.Title style={{fontWeight:"bold", marginTop:"20px", color: "#0C2D57"}}>3D Food Printing Mobile Application</Card.Title>
-                                <Card.Link style={{textDecoration:"none", fontWeight:"500", fontSize:"15px", color:"#ff9933"}} href="https://www.figma.com/file/NshEau6BrO6uVld9NHiQqE/Website?type=design&node-id=167-1424&mode=design">See more..</Card.Link>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                </div>
+                
+            <h1 className="project-heading">Projects</h1> {/* Updated heading */}
+             <MDBRow className='row-cols-1 row-cols-md-2 g-4 project-col'>
+                <MDBCol>
+                <MDBCard className="card">
+                <a href="https://www.figma.com/file/NshEau6BrO6uVld9NHiQqE/Website?type=design&node-id=83-176&mode=design" target="_blank" rel="noopener noreferrer">
+                <MDBCardImage
+                    src={image1}
+                    alt='...'
+                    position='top'
+                />
+                </a>
+                
+                <MDBCardBody>
+                    <MDBCardTitle className="project-des">Ezthara Hair Miracle - Redesigned the UI of Ezthara</MDBCardTitle>
+                    {/* <MDBCardText>
+                    This is a longer card with supporting text below as a natural lead-in to additional content.
+                    This content is a little bit longer.
+                    </MDBCardText> */}
+                </MDBCardBody>
+                </MDBCard>
+            </MDBCol>
+            <MDBCol>
+            <MDBCard className="card">
+            <a href="https://www.figma.com/file/LAiqAojHVT9xcNFRA9gCkz/RevolUX?type=design&node-id=0%3A1&mode=design&t=ge2bQLo9LcW5VT6g-1" target="_blank" rel="noopener noreferrer">
+                <MDBCardImage
+                    src={image2}
+                    alt='...'
+                    position='top'
+                />
+                </a>
+                
+                <MDBCardBody>
+                    <MDBCardTitle className="project-des">BIT LEARN - Cryptocurrency Learning Application</MDBCardTitle>
+                    {/* <MDBCardText>
+                    This is a longer card with supporting text below as a natural lead-in to additional content.
+                    This content is a little bit longer.
+                    </MDBCardText> */}
+                </MDBCardBody>
+                </MDBCard>
+            </MDBCol>
+            <MDBCol>
+            <MDBCard className="card">
+            <a href="https://www.figma.com/file/NshEau6BrO6uVld9NHiQqE/Website?type=design&node-id=167-1424&mode=design" target="_blank" rel="noopener noreferrer">
+                <MDBCardImage
+                    src={image3}
+                    alt='...'
+                    position='top'
+                />
+                </a>
+                
+                <MDBCardBody>
+                    <MDBCardTitle className="project-des">3D Food Printing Mobile Application</MDBCardTitle>
+                    {/* <MDBCardText>
+                    This is a longer card with supporting text below as a natural lead-in to additional content.
+                    This content is a little bit longer.
+                    </MDBCardText> */}
+                </MDBCardBody>
+                </MDBCard>
+            </MDBCol>
+            <MDBCol>
+            <MDBCard className="card">
+                <a href="https://www.figma.com/file/NshEau6BrO6uVld9NHiQqE/Website?type=design&node-id=83-176&mode=design" target="_blank" rel="noopener noreferrer">
+                <MDBCardImage
+                    src={image1}
+                    alt='...'
+                    position='top'
+                />
+                </a>
+                
+                <MDBCardBody>
+                    <MDBCardTitle className="project-des">Ezthara Hair Miracle - Redesigned the UI of Ezthara</MDBCardTitle>
+                    {/* <MDBCardText>
+                    This is a longer card with supporting text below as a natural lead-in to additional content.
+                    This content is a little bit longer.
+                    </MDBCardText> */}
+                </MDBCardBody>
+                </MDBCard>
+            </MDBCol>
+            </MDBRow>
                   
-        </div>
-
         
-<div class="container">
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div class="col-md-4 d-flex align-items-center">
-      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-      </a>
-      <span class="mb-3 mb-md-0" style={{color:"#0C2D57", fontWeight:"400", fontSize:"10px"}}>© 2024 Copyright Senuvi Jayasinghe.</span>
-    </div>
 
-    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-    <li class="ms-3"><a class="text-muted" href="https://www.linkedin.com/in/senuvi-jayasinghe-414513256/"><img alt="" src={image5} width="20" height="20"/></a></li>
-    <li class="ms-3"><a class="text-muted" href="https://github.com/SENJ01"><img alt="" src={image6} width="20" height="20"/></a></li>
-    <li class="ms-3"><a class="text-muted" href="https://www.instagram.com/senuvi_j/?igsh=dGtsdDdsbmQ1YzR4&utm_source=qr"><img alt="" src={image7} width="20" height="20"/></a></li>
-    </ul>
-  </footer>
-</div>
+                
+        <div class="container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <div class="col-md-4 d-flex align-items-center">
+            <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+            </a>
+            <span class="mb-3 mb-md-0" style={{color:"#0C2D57", fontWeight:"400", fontSize:"10px"}}>© 2024 Copyright Senuvi Jayasinghe.</span>
+            </div>
+
+            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li class="ms-3"><a class="text-muted" href="https://www.linkedin.com/in/senuvi-jayasinghe-414513256/"><img alt="" src={image5} width="20" height="20"/></a></li>
+            <li class="ms-3"><a class="text-muted" href="https://github.com/SENJ01"><img alt="" src={image6} width="20" height="20"/></a></li>
+            <li class="ms-3"><a class="text-muted" href="https://www.instagram.com/senuvi_j/?igsh=dGtsdDdsbmQ1YzR4&utm_source=qr"><img alt="" src={image7} width="20" height="20"/></a></li>
+            </ul>
+        </footer>
+        </div>
       </section>
     );
 }
 
-export default Home;
+export default Testing;
